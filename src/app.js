3a89fb532c0fe0c864522dbde6113d4803f3e67e
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-11-06 12:33:43 +0100

    red background and printing all countries to console

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+import axios from 'axios';
+
+//test
 console.log('Hallo daar!');
+
+//get array with all countries
+async function fetchData() {
+    try {
+        const result = await axios.get('https://restcountries.eu/rest/v2/all');
+        // const amount = result.data.amount;
+        // const name = result.data.name;
+        // const flag = result.data.flag;
+        // console.log(name + flag + `Has a population of ${amount} people`);
+        console.log(result);
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+fetchData();
