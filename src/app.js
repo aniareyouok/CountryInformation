4ea3929aa4c0675fd94deed588ad3e0986041a02
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-11-07 22:28:28 +0100

    hoover color effect corresponding with continent

--- src/app.js
+++ src/app.js
@@ -7,39 +7,54 @@ console.log('Hallo daar!');
 async function fetchData() {
     try {
         //retrieving array with country information of all countries
-            const result = await axios.get('https://restcountries.com/v2/all')
+        const result = await axios.get('https://restcountries.com/v2/all')
 
         //sorting the array on population size from small to big
         result.data.sort((a, b) => {
             return a.population - b.population;
         })
-            //mapping through array of all countries
-            const countryList = result.data.map((country) => {
+        //mapping through array of all countries
+        const countryList = result.data.map((country) => {
 
-                //for each country creating a div-tag called country
-                const countryDiv = document.createElement("div");
-                countryDiv.setAttribute("class", "country")
+            //for each country creating a div-tag called country
+            const countryDiv = document.createElement("div");
+            countryDiv.setAttribute("class", "country")
 
-                //within each div a img-tag showing the corresponding flag
-                const flagImg = document.createElement("img");
-                flagImg.setAttribute("src", country.flag)
-                flagImg.setAttribute("alt", `Flag of ${name}`);
-                countryDiv.appendChild(flagImg);
+            //within each div a img-tag showing the corresponding flag
+            const flagImg = document.createElement("img");
+            flagImg.setAttribute("src", country.flag)
+            flagImg.setAttribute("alt", `Flag of ${country.name}`);
+            countryDiv.appendChild(flagImg);
 
-                //a <p> with text is also added to the country-div
-                const infoP = document.createElement("p");
-                infoP.textContent = `${country.name} has a population of ${country.population} people.`;
-                countryDiv.appendChild(infoP)
+            //a <p> is added to the country-div
+            const infoP = document.createElement("p");
 
-                //each mapping wil place a country-div in the html container called country-box
-                    return document.getElementById("country-box").appendChild(countryDiv);
+            //class name for <p> is the same as corresponding region
+            infoP.setAttribute("class", country.region)
 
-            }); return countryList
-    } catch(e) {
+            //adding name of country and population-size to <p> and
+            //putting <p> into country-div
+            infoP.textContent = `${country.name} has a population of ${country.population} people.`;
+            countryDiv.appendChild(infoP)
+
+            //each mapping wil place a country-div in the html container called country-box
+            return document.getElementById("country-box").appendChild(countryDiv);
+
+        });
+        return countryList
+    } catch (e) {
         console.error(e);
     }
 }
 
-fetchData().then(r => {});
+fetchData().then(r => {
+});
+
+// De land-namen moeten worden weergegeven in een kleur die overeenkomt met het continent waar het land in ligt. Tip: maak hier een aparte functie voor die een regio-naam verwacht en bepaalt welke kleur het land moet krijgen. Een land ligt meestal in één van de volgende vijf contintenten, maar uitzonderingen kunnen voorkomen:
+//     Africa: blauw
+// Americas: groen
+// Asia: rood
+// Europe: geel
+// Oceania: paars
 
 
