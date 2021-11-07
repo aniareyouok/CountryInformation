7566ae980f47046f5b901ebe26f6303c50ab02df
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-11-07 22:43:12 +0100

    cleaning up

--- src/app.js
+++ src/app.js
@@ -50,11 +50,4 @@ async function fetchData() {
 fetchData().then(r => {
 });
 
-// De land-namen moeten worden weergegeven in een kleur die overeenkomt met het continent waar het land in ligt. Tip: maak hier een aparte functie voor die een regio-naam verwacht en bepaalt welke kleur het land moet krijgen. Een land ligt meestal in één van de volgende vijf contintenten, maar uitzonderingen kunnen voorkomen:
-//     Africa: blauw
-// Americas: groen
-// Asia: rood
-// Europe: geel
-// Oceania: paars
-
 
