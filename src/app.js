f77806a190a15998d83264ff6836ca33bbc44b61
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-11-07 21:30:36 +0100

    added comments to code for readability

--- src/app.js
+++ src/app.js
@@ -8,42 +8,29 @@ async function fetchData() {
     try {
         //retrieving array with country information of all countries
             const result = await axios.get('https://restcountries.com/v2/all')
+
+            //mapping through array of all countries
             const countryList = result.data.map((country) => {
+
+                //for each country creating a div-tag called country
                 const countryDiv = document.createElement("div");
                 countryDiv.setAttribute("class", "country")
+
+                //within each div a img-tag showing the corresponding flag
                 const flagImg = document.createElement("img");
                 flagImg.setAttribute("src", country.flag)
                 flagImg.setAttribute("alt", `Flag of ${name}`);
                 countryDiv.appendChild(flagImg);
 
+                //a <p> with text is also added to the country-div
                 const infoP = document.createElement("p");
-                infoP.setAttribute("class", "info");
                 infoP.textContent = `${country.name} has a population of ${country.population} people.`;
                 countryDiv.appendChild(infoP)
 
-                const countryItem = document.getElementById("country-box").appendChild(countryDiv);
+                //each mapping wil place a country-div in the html container called country-box
+                    return document.getElementById("country-box").appendChild(countryDiv);
 
-                return countryItem
             }); return countryList
-
-        // //looping through the array, for each country first defining name and population
-        //     const countryList = result.data.map((country) => {
-        //     const name = country.name;
-        //     const amount = country.population;
-        //
-        // //second, adding flag
-        //     const flagImg = document.createElement("img");
-        //     flagImg.setAttribute("src", country.flag)
-        //     flagImg.setAttribute("alt", `Flag of ${name}`)
-        //     const flag = document.getElementById("flag").appendChild(flagImg);
-        //
-        // // finally, adding text
-        //     const infoP = document.createElement("p");
-        //         infoP.textContent = `${name} has a population of ${amount} people`
-        //         const info = document.body.appendChild(infoP)
-        //     //return
-        //         return "<div class='info'><p>`${name} has a population of ${amount} people`</p></div>" ;
-
     } catch(e) {
         console.error(e);
     }
@@ -51,9 +38,4 @@ async function fetchData() {
 
 fetchData().then(r => {});
 
-// function allProducts(list) {
-//     const products = list.map((item) => {
-//         return '<div class="block">' + itemName(item) + '<br>' + itemPrice(item) + '<br>' + screenSizeInCm(item) + '</div>'
-//     });
-//     return products.join(" ");
 
