ac02813070ceac974371dab6291a884adb29fd67
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-11-12 00:10:25 +0100

    fetchCountry functie for belgie doet het

--- src/js/search.js
+++ src/js/search.js
@@ -1,46 +1,51 @@
 import axios from "axios";
 
-// const button = document.getElementById("search_button";
-// button.addEventListener("submit", searchCountry);
-//
-// async function searchCountry() {
-//     try {
-//         //retrieving array with country information of all countries
-//         const result = await axios.get('https://restcountries.com/v2/all') {
-//
-//             //determine country entered in search field
-//             const inputField = document.getElementById("search_field");
-//             function handleInput(event) {
-//                 inputField.addEventListener(onsubmit, ()=> {
-//
-//                 })
-//                 return event.target.value
-//                 }
-//
-//             }
-//
-//
-//
-//
-//             //find and show country information entered in search field
-//
-//
-//
-//         } return countryInformation;
-//     } catch (e) {
-//         console.error(e);
-//     }
-//
-// }
-//
-// searchCountry();
-//
+//check the array in the console
+async function test() {
+    try {
+        const result = await axios.get('https://restcountries.com/v2/name/belgie');
+        console.log(result);
+        console.log(result.data[0].flag + " " + result.data[0].name + " " + result.data[0].subregion + " " + result.data[0].population + " " + result.data[0].capital);
+        console.log(result.data[0].currencies)
+        console.log(result.data[0].languages[0].name + " and " + result.data[0].languages[1].name)
+    } catch  (error) {
+        console.error(error);
+    }};
+
+    test();
+
+//Retrieves flag, name, subregion, population and capital and invokes the getCurrency and getLanguage function
+async function fetchCountry() {
+    try {
+        const result = await axios.get('https://restcountries.com/v2/name/belgie');
+        const flag = `<img src="${result.data[0].flag}" alt="Flag of ${result.data[0].name}">`
+        const country_name = result.data[0].name;
+        const subarea_name = result.data[0].subregion;
+        const amount = result.data[0].population;
+        const city = result.data[0].capital;
+        const currencies = result.data[0].currencies;
+        const currency = getCurrency(currencies);
+
+        let outcome = `<div class="flag_name">
+        ${flag} <h1>${country_name}</h1></div> <br>
+        <div class="text"><p>${country_name} is situated in ${subarea_name}. <br>
+        It has a population of ${amount} people. <br>
+        The capital is ${city} and ${currency} <br>
+        </p></div>`
+
+        document.getElementById("outcome_search").innerHTML = outcome;
+
+    } catch (error) {
+    console.error(error);
+}}
+
+fetchCountry();
+
+// ${getLanguage}
 
 //Retrieves the kind of currencies you can pay with in a given country. This function gets evoked by the fetchData function.
-async function getCurrency() {
+function getCurrency(currencies) {
     try {
-        const result = await axios.get('https://restcountries.eu/rest/v2/alpha/be');
-        const currencies = result.data.currencies;
         let allCurrenciesArray = [];
 
         for (let i = 0; i < currencies.length; i++) {
@@ -48,16 +53,18 @@ async function getCurrency() {
         }
 
         if (currencies.length === 1) {
-            console.log( `You can pay with ${currencies[0].name}'s`);
+            return `you can pay with ${currencies[0].name}'s`;
         } else if (currencies.length < 1) {
             const lastCurrency = allCurrenciesArray.pop()
             const allCurrencies = allCurrenciesArray.join(", ");
-            console.log( `You can pay with ${allCurrencies} and ${lastCurrency}.`);
+            return `you can pay with ${allCurrencies} and ${lastCurrency}.`;
         } else {
-            console.log(`You can't by anything here, use your skills and help each other out`);
+            return `you can't by anything here, use your skills and help each other out`;
         }
-
     } catch (error) {
         console.error(error);
     }
 }
+
+// //Retrieves languages
+// data[0].languages[i].name;
